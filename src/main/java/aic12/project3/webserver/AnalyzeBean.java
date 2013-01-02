ee49c3e324e978dec4896b74943eaadbb71fc56c
Michael Heil <michael.heil@chello.at>
2013-01-02 18:03:36 +0100

    Sentiment analysis with tasks

--- src/main/java/aic12/project3/webserver/AnalyzeBean.java
+++ src/main/java/aic12/project3/webserver/AnalyzeBean.java
@@ -1,12 +1,15 @@
 package aic12.project3.webserver;
 
+import static com.google.appengine.api.taskqueue.TaskOptions.Builder.withUrl;
+
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import aic12.project3.analysis.service.SentimentRequest;
-import aic12.project3.analysis.service.SentimentService;
-
-import java.io.Serializable;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
 
 @ManagedBean
 @SessionScoped
@@ -17,17 +20,17 @@ public class AnalyzeBean implements Serializable
 
     public String analyze() throws Exception
     {
-        SentimentRequest req = new SentimentRequest();
-        req.setCompanyName(companyName);
+        Queue queue = QueueFactory.getDefaultQueue();
 
-        SentimentService service = new SentimentService();
-        service.analyze(req);
+        for (int i = 0; i < 10; i++)
+        {
+            queue.add(withUrl("/tasks/analysis").method(TaskOptions.Method.GET).param("company", companyName));
+        }
 
-        double interval = 1.96 * Math.sqrt(req.getSentiment() * (1 - req.getSentiment()) / (req.getNumberOfTweets() - 1));
-
-        result = "Amount: " + req.getNumberOfTweets() + " - Sentiment: (" + (req.getSentiment() - interval) + " < " + req.getSentiment() + " < " + (req.getSentiment() + interval)
-                + ")";
-
+//        double interval = 1.96 * Math.sqrt(req.getSentiment() * (1 - req.getSentiment()) / (req.getNumberOfTweets() - 1));
+//
+//        result = "Amount: " + req.getNumberOfTweets() + " - Sentiment: (" + (req.getSentiment() - interval) + " < " + req.getSentiment() + " < " + (req.getSentiment() + interval)
+//                + ")";
         return "result.xhtml";
     }
 
@@ -45,5 +48,4 @@ public class AnalyzeBean implements Serializable
     {
         return result;
     }
-
 }
--- src/main/java/aic12/project3/webserver/servlet/AnalysisServlet.java
+++ src/main/java/aic12/project3/webserver/servlet/AnalysisServlet.java
@@ -1,24 +1,30 @@
-package aic12.project3.analysis.service;
+package aic12.project3.webserver.servlet;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Tweet;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+import aic12.project3.analysis.service.SentimentRequest;
 import classifier.ClassifierBuilder;
 import classifier.IClassifier;
 import classifier.WeightedMajority;
 import classifier.WekaClassifier;
 
-public class SentimentService
+public class AnalysisServlet extends HttpServlet
 {
-    private WeightedMajority wm;
+    private static WeightedMajority wm;
 
-    public SentimentService() throws Exception
+    public AnalysisServlet() throws Exception
     {
         // Pre chaching of neural networks
         List<IClassifier> classifiers = new LinkedList<IClassifier>();
@@ -35,18 +41,17 @@ public class SentimentService
         wm.weightedClassify("test");
     }
 
-    public void analyze(SentimentRequest request) throws Exception
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
     {
-        request.setTimestampStartOfAnalysis(System.currentTimeMillis());
-        
         List<Tweet> tweets = new ArrayList<Tweet>(1000);
-        
+
         // TODO: query database for tweets
         // so far Twitter4J is used
+        try
         {
             Twitter twitter = new TwitterFactory().getInstance();
 
-            Query query = new Query(request.getCompanyName());
+            Query query = new Query(req.getParameter("company"));
             query.setLang("en");
             query.setRpp(100);
 
@@ -56,10 +61,12 @@ public class SentimentService
                 QueryResult result = twitter.search(query);
                 tweets.addAll(result.getTweets());
             }
+
+            SentimentRequest request = new SentimentRequest();
+            calculateSentiment(request, tweets);
         }
-
-        calculateSentiment(request, tweets);
-
+        catch (Exception e)
+        {}
     }
 
     private void calculateSentiment(SentimentRequest request, List<Tweet> list) throws Exception
@@ -76,5 +83,7 @@ public class SentimentService
         request.setNumberOfTweets(list.size());
 
         request.setTimestampAnalyzed(System.currentTimeMillis());
+        
+        // TODO: store into DB
     }
 }
