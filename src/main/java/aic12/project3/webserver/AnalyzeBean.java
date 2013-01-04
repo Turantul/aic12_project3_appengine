a7672a1d2271f08c24b2baa5dd87a50e4c0029cc
Michael Heil <michael.heil@chello.at>
2013-01-04 16:36:37 +0100

    Initial version with polling

--- src/main/java/aic12/project3/webserver/AnalyzeBean.java
+++ src/main/java/aic12/project3/webserver/AnalyzeBean.java
@@ -25,6 +25,8 @@ public class AnalyzeBean implements Serializable
 {
     private String companyName;
     private String result;
+    private int parts;
+    private String key;
 
     public String analyze() throws Exception
     {
@@ -34,39 +36,47 @@ public class AnalyzeBean implements Serializable
         request.setFrom(new Date());
         request.setTo(new Date());
         request.setTimestampRequestSending(System.currentTimeMillis());
+        
+        //TODO: set correctly
         request.setParts(10);
+        parts = 10;
 
         Key<SentimentRequestDTO> key = GoogleRequestDAO.instance.saveRequest(request);
+        this.key = key.getString();
 
+        //TODO: enable a reasonable splitting mechanism based on tweet amount
         Queue queue = QueueFactory.getDefaultQueue();
-
         for (int i = 0; i < 10; i++)
         {
             queue.add(withUrl("/tasks/analysis").method(TaskOptions.Method.GET).param("request", key.getString()));
         }
         
-        List<SentimentProcessingRequestDTO> list;
-        do
-        {
-            Thread.sleep(5000);
-            list = GoogleProcessingRequestDAO.instance.getAllSentimentRequestForRequest(key.getString());
-            System.out.println(list.size());
-        } while (list.size() < 10);
-        
-        float sentiment = 0;
-        int amount = 0;
-        for (SentimentProcessingRequestDTO pro : list)
-        {
-            amount += pro.getNumberOfTweets();
-            sentiment += pro.getSentiment() * pro.getNumberOfTweets();
-        }
-        
-        sentiment /= amount;
-
-        double interval = 1.96 * Math.sqrt(sentiment * (1 - sentiment) / (amount - 1));
-        
-        result = "Amount: " + amount + " - Sentiment: (" + (sentiment - interval) + " < " + sentiment + " < " + (sentiment + interval) + ")";
         return "result.xhtml";
+    }
+    
+    public void poll()
+    {
+        List<SentimentProcessingRequestDTO> list = GoogleProcessingRequestDAO.instance.getAllSentimentRequestForRequest(key);
+        if (list.size() < parts)
+        {
+            result = "Calculating: " + list.size() + "/" + parts;
+        }
+        else
+        {
+            float sentiment = 0;
+            int amount = 0;
+            for (SentimentProcessingRequestDTO pro : list)
+            {
+                amount += pro.getNumberOfTweets();
+                sentiment += pro.getSentiment() * pro.getNumberOfTweets();
+            }
+            
+            sentiment /= amount;
+    
+            double interval = 1.96 * Math.sqrt(sentiment * (1 - sentiment) / (amount - 1));
+            
+            result = "Amount: " + amount + " - Sentiment: (" + (sentiment - interval) + " < " + sentiment + " < " + (sentiment + interval) + ")";
+        }
     }
 
     public String getCompanyName()
